6a9c1f513d4297be5f013c7f8e8d8b4d907ca4d2
Sokolovskii Ivan <ivan3177@gmail.com>
2025-11-28 23:58:46 +0300

    feat(reassure): тесты производительности компонента RadioButton

---
+++ src/components/RadioButton/__tests__/RadioButton.perf-test.tsx
@@ -0,0 +1,35 @@
+import { fireEvent } from '@testing-library/react-native'
+
+import { measureComponentPerformance } from '../../../utils/__tests__/perf-utils'
+import { RadioButton, type RadioButtonProps } from '../RadioButton'
+
+const states: Array<RadioButtonProps['state']> = ['default', 'danger']
+const checkedStates: boolean[] = [true, false]
+const disabledStates: boolean[] = [true, false]
+
+describe('RadioButton performance', () => {
+  for (const state of states) {
+    for (const checked of checkedStates) {
+      for (const disabled of disabledStates) {
+        test(`state: ${state}, checked: ${checked}, disabled: ${disabled}`, async () => {
+          await measureComponentPerformance(
+            <RadioButton
+              checked={checked}
+              disabled={disabled}
+              state={state}
+              onPress={jest.fn()}
+            />
+          )
+        })
+      }
+    }
+  }
+
+  test('Interaction', async () => {
+    await measureComponentPerformance(<RadioButton onPress={jest.fn()} />, {
+      scenario: async ({ getByTestId }) => {
+        fireEvent.press(getByTestId('radio-button'))
+      },
+    })
+  })
+})
