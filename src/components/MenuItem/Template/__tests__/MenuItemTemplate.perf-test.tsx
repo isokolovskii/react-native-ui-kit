97400ad54005a5eb73af579819dd7336d4a3b1bf
Sokolovskii Ivan <ivan3177@gmail.com>
2025-11-28 23:34:06 +0300

    feat(reassure): тесты производительности компонента MenuItemTemplate

---
+++ src/components/MenuItem/Template/__tests__/MenuItemTemplate.perf-test.tsx
@@ -0,0 +1,48 @@
+import { IconUser, IconChevronRight } from '@tabler/icons-react-native'
+import { fireEvent, screen } from '@testing-library/react-native'
+
+import { measureComponentPerformance } from '../../../../utils/__tests__/perf-test-utils'
+import { MenuItemTemplate } from '../MenuItemTemplate'
+
+describe('MenuItemTemplate performance', () => {
+  test('minimal menu item', async () => {
+    await measureComponentPerformance(<MenuItemTemplate title='Minimal' />)
+  })
+
+  test('menu item with icon and badge', async () => {
+    await measureComponentPerformance(
+      <MenuItemTemplate
+        Icon={IconUser}
+        badgeSeverity='warning'
+        title='With Icon and Badge'
+      />
+    )
+  })
+
+  test('menu item with accessories', async () => {
+    await measureComponentPerformance(
+      <MenuItemTemplate
+        PrefixIcon={IconChevronRight}
+        SuffixIcon={IconChevronRight}
+        title='With Accessories'
+      />
+    )
+  })
+
+  test('disabled menu item', async () => {
+    await measureComponentPerformance(
+      <MenuItemTemplate disabled title='Disabled' />
+    )
+  })
+
+  test('press interaction', async () => {
+    const scenario = async () => {
+      fireEvent.press(screen.getByText('Pressable'))
+    }
+
+    await measureComponentPerformance(
+      <MenuItemTemplate title='Pressable' onPress={jest.fn()} />,
+      { scenario }
+    )
+  })
+})
