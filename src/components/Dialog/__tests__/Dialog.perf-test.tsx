49872e919a7fe50ad420c7cfd29a258c28f15fbf
Sokolovskii Ivan <ivan3177@gmail.com>
2025-11-28 23:27:45 +0300

    feat(reassure): тесты производительности компонента Dialog

---
+++ src/components/Dialog/__tests__/Dialog.perf-test.tsx
@@ -0,0 +1,46 @@
+import { PortalProvider } from '@gorhom/portal'
+import { fireEvent, screen } from '@testing-library/react-native'
+import { useState } from 'react'
+
+import { Button, Text, View } from 'react-native'
+
+import { measureComponentPerformance } from '../../../utils/__tests__/perf-test-utils'
+import { Dialog } from '../Dialog'
+import { DialogHeader } from '../DialogHeader'
+
+const DialogTestComponent = () => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const body = () => <Text>Dialog Body</Text>
+
+  return (
+    <PortalProvider>
+      <View>
+        <Button title='Show Dialog' onPress={() => setIsVisible(true)} />
+        <Dialog
+          body={body}
+          header={
+            <DialogHeader
+              title='Test Dialog'
+              onClose={() => setIsVisible(false)}
+            />
+          }
+          isVisible={isVisible}
+          onClose={() => setIsVisible(false)}
+        />
+      </View>
+    </PortalProvider>
+  )
+}
+
+describe('Dialog performance', () => {
+  test('show and hide dialog', async () => {
+    const scenario = async () => {
+      fireEvent.press(screen.getByText('Show Dialog'))
+      await screen.findByTestId('DialogCloseButton')
+      fireEvent.press(screen.getByTestId('DialogCloseButton'))
+    }
+
+    await measureComponentPerformance(<DialogTestComponent />, { scenario })
+  })
+})
