862beade0d30de4dccf4a00794a8b4930fa3dc4b
Sokolovskii Ivan <ivan3177@gmail.com>
2025-11-28 23:28:53 +0300

    feat(reassure): тесты производительности компонента Divider

---
+++ src/components/Divider/__tests__/Divider.perf-test.tsx
@@ -0,0 +1,28 @@
+import { IconCircleCheck } from '@tabler/icons-react-native'
+
+import { measureComponentPerformance } from '../../../utils/__tests__/perf-test-utils'
+import { Divider } from '../Divider'
+
+describe('Divider performance', () => {
+  test('simple horizontal divider', async () => {
+    await measureComponentPerformance(<Divider />)
+  })
+
+  test('horizontal divider with text', async () => {
+    await measureComponentPerformance(<Divider text='Divider Text' />)
+  })
+
+  test('horizontal divider with text and icon', async () => {
+    await measureComponentPerformance(
+      <Divider Icon={IconCircleCheck} text='Divider Text' />
+    )
+  })
+
+  test('vertical divider', async () => {
+    await measureComponentPerformance(<Divider layout='vertical' />)
+  })
+
+  test('dashed divider', async () => {
+    await measureComponentPerformance(<Divider type='dash' />)
+  })
+})
