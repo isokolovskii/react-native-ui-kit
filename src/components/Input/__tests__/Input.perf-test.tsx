bfc2c7cdaf2378772361e95072c573429bad800f
Sokolovskii Ivan <ivan3177@gmail.com>
2025-11-28 23:30:47 +0300

    feat(reassure): тесты производительности компонента Input

---
+++ src/components/Input/__tests__/Input.perf-test.tsx
@@ -0,0 +1,55 @@
+import { fireEvent, screen } from '@testing-library/react-native'
+
+import { measureComponentPerformance } from '../../../utils/__tests__/perf-test-utils'
+import { InputGroup } from '../InputGroup'
+import { InputText } from '../InputText'
+
+describe('Input performance', () => {
+  describe('InputText', () => {
+    test('simple input', async () => {
+      await measureComponentPerformance(
+        <InputText placeholder='Simple Input' />
+      )
+    })
+
+    test('disabled input', async () => {
+      await measureComponentPerformance(
+        <InputText disabled placeholder='Disabled Input' />
+      )
+    })
+
+    test('danger input', async () => {
+      await measureComponentPerformance(
+        <InputText placeholder='Danger Input' state='danger' />
+      )
+    })
+
+    test('floating label input', async () => {
+      await measureComponentPerformance(
+        <InputText floatLabel placeholder='Floating Label' />
+      )
+    })
+
+    test('typing interaction', async () => {
+      const scenario = async () => {
+        fireEvent.changeText(
+          screen.getByPlaceholderText('Typing Test'),
+          'Hello World'
+        )
+      }
+
+      await measureComponentPerformance(
+        <InputText placeholder='Typing Test' />,
+        { scenario }
+      )
+    })
+  })
+
+  describe('InputGroup', () => {
+    test('input with addons', async () => {
+      await measureComponentPerformance(
+        <InputGroup left='L' placeholder='Input with Addons' right='R' />
+      )
+    })
+  })
+})
