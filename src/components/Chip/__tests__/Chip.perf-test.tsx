b6d270b4f79f0a2b1c5a3168e882e8573a55359f
Sokolovskii Ivan <ivan3177@gmail.com>
2025-11-28 23:25:26 +0300

    feat(reassure): тесты производительности компонента Chip

---
+++ src/components/Chip/__tests__/Chip.perf-test.tsx
@@ -0,0 +1,49 @@
+import { IconUser } from '@tabler/icons-react-native'
+import { fireEvent, screen } from '@testing-library/react-native'
+
+import { measureComponentPerformance } from '../../../utils/__tests__/perf-test-utils'
+import { Chip, TestId } from '../Chip'
+
+describe('Chip performance', () => {
+  test('simple chip', async () => {
+    await measureComponentPerformance(<Chip label='Simple Chip' />)
+  })
+
+  test('chip with icon', async () => {
+    await measureComponentPerformance(
+      <Chip Icon={IconUser} label='Chip with Icon' />
+    )
+  })
+
+  test('chip with close button', async () => {
+    await measureComponentPerformance(
+      <Chip label='Chip with Close' onClose={jest.fn()} />
+    )
+  })
+
+  test('disabled chip', async () => {
+    await measureComponentPerformance(<Chip disabled label='Disabled Chip' />)
+  })
+
+  test('press interaction', async () => {
+    const scenario = async () => {
+      fireEvent.press(screen.getByTestId(TestId.Container))
+    }
+
+    await measureComponentPerformance(
+      <Chip label='Pressable Chip' onPress={jest.fn()} />,
+      { scenario }
+    )
+  })
+
+  test('close press interaction', async () => {
+    const scenario = async () => {
+      fireEvent.press(screen.getByTestId(TestId.RemoveButton))
+    }
+
+    await measureComponentPerformance(
+      <Chip label='Closable Chip' onClose={jest.fn()} />,
+      { scenario }
+    )
+  })
+})
