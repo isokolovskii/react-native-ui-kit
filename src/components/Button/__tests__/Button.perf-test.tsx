b4390840f608a64cf3a786f8ba63997a2446373c
Sokolovskii Ivan <ivan3177@gmail.com>
2025-11-28 23:22:44 +0300

    feat(reassure): тесты производительности компонента Button

---
+++ src/components/Button/__tests__/Button.perf-test.tsx
@@ -0,0 +1,34 @@
+import { IconArrowDownRight } from '@tabler/icons-react-native'
+
+import { measureComponentPerformance } from '../../../utils/__tests__/perf-test-utils'
+import { Button } from '../Button'
+
+describe('Button performance', () => {
+  test('primary button', async () => {
+    await measureComponentPerformance(
+      <Button label='Primary Button' variant='primary' />
+    )
+  })
+
+  test('button with icon', async () => {
+    await measureComponentPerformance(
+      <Button
+        Icon={IconArrowDownRight}
+        label='Button with Icon'
+        variant='primary'
+      />
+    )
+  })
+
+  test('disabled button', async () => {
+    await measureComponentPerformance(
+      <Button disabled label='Disabled Button' variant='primary' />
+    )
+  })
+
+  test('loading button', async () => {
+    await measureComponentPerformance(
+      <Button loading label='Loading Button' variant='primary' />
+    )
+  })
+})
